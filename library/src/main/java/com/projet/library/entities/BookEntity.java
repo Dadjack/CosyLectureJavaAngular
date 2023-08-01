ad589342c2ec9f2ed1d769dea768f03d9492e279
Dadjack <88081348+Dadjack@users.noreply.github.com>
2023-08-02 00:09:33 +0200

    first entity

---
+++ library/src/main/java/com/projet/library/entities/BookEntity.java
@@ -0,0 +1,12 @@
+package com.projet.library.entities;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class BookEntity {
+    
+}
