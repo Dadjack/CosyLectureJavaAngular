4299b2a9abc1e22fc0621fd60423f757749ec0d4
bplaniteye <bplaniteye.pro@outlook.fr>
2023-08-08 17:08:43 +0200

    Set API REST request for Category

--- frontEndLibray/frontEnd/src/app/app.module.ts
+++ frontEndLibray/frontEnd/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { FormsModule } from '@angular/forms';
 import { ErrorComponent } from './error/error.component';
 import { HeaderComponent } from './header/header.component';
 import { HttpClientModule } from '@angular/common/http';
+import { CategoryComponent } from './category/category.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { HttpClientModule } from '@angular/common/http';
     AdministratorComponent,
     AddBookComponent,
     ErrorComponent,
-    HeaderComponent
+    HeaderComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
