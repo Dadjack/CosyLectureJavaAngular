978b093312d362469883c8235e6ee8d3b8a625c1
UgoBit <ugo.bittoni@gmail.com>
2023-08-08 17:11:36 +0200

    Creating books templates + pagination

--- frontEndLibray/frontEnd/src/app/app.module.ts
+++ frontEndLibray/frontEnd/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +12,8 @@ import { FormsModule } from '@angular/forms';
 import { ErrorComponent } from './error/error.component';
 import { HeaderComponent } from './header/header.component';
 import { HttpClientModule } from '@angular/common/http';
+
+
 
 @NgModule({
   declarations: [
@@ -25,6 +28,7 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     FormsModule,
+    NgxPaginationModule,
     HttpClientModule,
     AppRoutingModule
   ],
