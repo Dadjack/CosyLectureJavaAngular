e9a8c68b2783051bbaf055f8e2282ae185f481ad
bplaniteye <bplaniteye.pro@outlook.fr>
2023-08-08 16:13:25 +0200

    Set RESt respônse in controllers

--- backEndLibrary/src/main/java/com/projet/library/controllers/LibraryUserController.java
+++ backEndLibrary/src/main/java/com/projet/library/controllers/LibraryUserController.java
@@ -1,9 +1,25 @@
 package com.projet.library.controllers;
 
+import com.projet.library.entities.LibraryUserEntity;
+import com.projet.library.repositories.LibraryUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
+
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class LibraryUserController {
+    @Autowired
+    LibraryUserRepository libraryUserRepository;
+
+
+    public ResponseEntity<Collection<LibraryUserEntity>> getAllLibraryUsers() {
+        return new ResponseEntity<>(libraryUserRepository.findAll(), HttpStatus.OK);
+    }
     
     //Method pour emprunter un 
 }
