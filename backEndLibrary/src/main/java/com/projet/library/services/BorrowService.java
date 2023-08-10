144ada30a30181f7cc7aaa2bdfa9cd827175d5a5
bplaniteye <bplaniteye.pro@outlook.fr>
2023-08-10 10:17:14 +0200

    BorrowService in progress

--- backEndLibrary/src/main/java/com/projet/library/services/BorrowService.java
+++ backEndLibrary/src/main/java/com/projet/library/services/BorrowService.java
@@ -1,5 +1,6 @@
-package com.projet.library.services;
+/*package com.projet.library.services;
 
+import com.projet.library.entities.BookEntity;
 import com.projet.library.entities.BorrowEntity;
 import org.springframework.stereotype.Service;
 
@@ -9,22 +10,25 @@ import java.time.temporal.ChronoUnit;
 @Service
 public class BorrowService {
     BorrowEntity borrow = new BorrowEntity();
+    BookEntity book = new BookEntity();
 
-  public long calculateDuration(BorrowEntity borrow) {
-      return ChronoUnit.DAYS.between(borrow.getStartDate(), borrow.getEndDate());
-  }
+
+    public long calculateDuration(BorrowEntity borrow) {
+        long duration = ChronoUnit.DAYS.between(borrow.getStartDate(), LocalDateTime.now())
+    }
 
     public long calculateDelay(BorrowEntity borrow) {
         return ChronoUnit.DAYS.between(borrow.getEndDate(), LocalDateTime.now());
     }
 
-  public float calculatePenalty(BorrowEntity borrow) {
-      long duration = calculateDuration(borrow);
-      float amount = 0;
-      while (duration > 30) {
-          amount += 0.5;
-          duration++;
-      }
+    public float calculatePenalty(BorrowEntity borrow) {
+        long duration = calculateDuration(borrow);
+        float amount = 0;
+        while (duration > 30) {
+            amount += 0.5;
+            duration++;
+        }
         return amount;
-  }
+    }
 }
+*/
